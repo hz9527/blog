db85a8de05f25a5773791700be8374a35712013b
hz9527 <981379316@qq.com>
2017-02-19 22:23:39 +0800

    nothing

--- app/models/blogListModel.js
+++ app/models/blogListModel.js
@@ -4,9 +4,13 @@ var BlogListSchema = new mongoose.Schema({
 	_id: String,//l+i
 	limit: Number,
 	key: String,
+	title: String,//标题
+	type: String,//分类
+	keyWord: String,//关键词
+	viewCount: {type: Number, default:0},
+	state: {type:Number, default:0},//状态，默认为删除状态，1为可见，0为不可见
 	blog: {type: mongoose.Schema.Types.String, ref:'Blog'},
 	comments: {type: mongoose.Schema.Types.String, ref:'Comments'},
-	viewCount: Number,
 	anonymous: {type: Boolean, default:false},//是否匿名，如果匿名，根据key计算bid，bname与purl
 	aId: {type: mongoose.Schema.Types.Number, ref:'User'}
 });
